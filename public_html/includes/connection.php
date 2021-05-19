5318fe8d6d3dcc6fec6c59f188dc72daf39c0366
renanrodriguez <orttzrenan@gmail.com>
2021-05-19 11:53:57 -0300

    mysqli

--- public_html/includes/connection.php
+++ public_html/includes/connection.php
@@ -10,8 +10,7 @@
 	//EXTERNA
 	
 	function conecta(){
-		$conec = mysql_connect("rinplay.mysql.dbaas.com.br","rinplay","R1i2n3p4@") or die("Erro ao conectar com o banco de dados");
-		mysql_select_db("rinplay", $conec) or die("Erro ao selecionar base de dados");
+		$conec = new mysqli("us-cdbr-east-03.cleardb.com","b579fb33a9fe9a","431ec88b", "heroku_2fbedc967eed9e1" ) or die("Erro ao conectar com o banco de dados");
 		return $conec;
 	}
 
@@ -20,4 +19,4 @@
 	unset($conec);
 	}
 
-?>
+?>	
