819c692f4465335fde724077e390c29f936e4a74
renanrodriguez <orttzrenan@gmail.com>
2021-05-19 14:16:03 -0300

    tuto

--- public_html/tutoriais.php
+++ public_html/tutoriais.php
@@ -2,14 +2,7 @@
 	ob_start();
 	session_start();
 	
-	if(($_SESSION['logado'] == "") or (!isset($_SESSION['logado']))):
-		header("Location: index.php");
-	endif;
 
-	include("includes/config.php"); 
-	include("includes/connection.php"); 
-	include("includes/funcoes.php"); 
-	$cx = conecta();
 	
 	$acao = LimpaEntrada($_REQUEST['acao']);
 
@@ -51,31 +44,7 @@ function fechaComentario( codigo ){
             <h3 class="titulo-sessao">Tutoriais</h3>
 			
             <table width="100%" border="0" cellpadding="0" cellspacing="0">
-            <?php
-			// BUSCA 
-			$s = "SELECT * FROM rp_tutoriais ORDER BY data DESC";
-			$r = mysql_query($s, $cx);
-			$consulta = "SELECT COUNT(*) FROM rp_tutoriais"; 	
-			if(mysql_num_rows($r) > 0):
-				while($ln = mysql_fetch_assoc($r)){
-					if($ln['arquivo'] == ""):
-						$link = "ver-tutorial.php?codigo=".$ln['codigo'];
-						$target = "_self";
-					else:
-						$link = "sgc/uploads/".$ln['arquivo'];
-						$target = "_blank"; 
-					endif;
-					?>
-                    <tr>
-                        <td class="linha-rodape-td">
-                            <img src="img/ico.materias.gif" border="0" align="texttop" alt=""/>
-                            <a target="<?php echo $target; ?>" href="<?php echo $link; ?>"> <?php echo $ln['titulo']; ?></a> - <?php echo $ln['data']; ?>
-                        </td>                                        
-                    </tr>
-					<?php        
-					}
-			endif;
-			?>
+           
             </table>
                     
         </div>
